175f3bf8164a859d4f57d25a95c9046cea4875f3
shivam singh <ssultanpura9@gmail.com>
2024-01-10 22:13:04 -0800

    api updated

--- index.js
+++ index.js
@@ -1,16 +1,36 @@
-const express = require('express');
+const express = require("express");
+
+const requestIp = require("request-ip");
+
+// inside middleware handler
+
 const app = express();
+var ipMiddleware = function(req, res, next) {
+    const clientIp = requestIp.getClientIp(req); 
+    next();
+   };
+   //As Connect Middleware
+   app.use(requestIp.mw())
+app.get("/api/whoami", (req, res) => {
+//   const ipAddress = req.clientIp;
+//   const language = req.headers["accept-language"];
+//   const software = req.headers["user-agent"];
+//   console.log(req.headers);
+//   res.json({
+//     ipaddress: ipAddress,
+//     language: language,
+//     software: software,
+//   });
 
-app.get('/api/whoami', (req, res) => {
-  const ipAddress = req.ip;
-  const language = req.headers['accept-language'];
-  const software = req.headers['user-agent'];
-
-  res.json({
-    ipaddress: ipAddress,
-    language: language,
-    software: software
-  });
+    var ipadress = req.clientIp;
+    var language = req.acceptsLanguages();
+    var software=req.get('User-Agent');
+     res.json({
+     ipadress: ipadress,
+     language:language[0],
+     software:software
+     });
+    
 });
 
 const PORT = process.env.PORT || 3000;
